60ca412ae97282550f88d507080bffff5485fa3b
tjclawson <tjamesclawson@gmail.com>
2020-02-27 19:17:28 -0500

    Add max_heap solution

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,19 +3,47 @@ class Heap:
         self.storage = []
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
-        pass
+        # swaps root and last element
+        self.storage[0], self.storage[len(self.storage) - 1] = self.storage[len(self.storage) - 1], self.storage[0]
+        # delete last element which is now the max
+        deleted_value = self.storage.pop()
+        # sifts down the new root element to ensure heap property is maintained
+        self._sift_down(0)
+        return deleted_value
 
     def get_max(self):
-        pass
+        return self.storage[0]
 
     def get_size(self):
-        pass
+        return len(self.storage)
 
     def _bubble_up(self, index):
-        pass
+        parent_index = (index - 1) // 2
+        if parent_index >= 0 and self.storage[index] > self.storage[parent_index]:
+            self.storage[index], self.storage[parent_index] = self.storage[parent_index], self.storage[index]
+            return self._bubble_up(parent_index)
 
     def _sift_down(self, index):
-        pass
+        bigger_child = self._get_bigger_child(index)
+        if bigger_child is not None and bigger_child > self.storage[index]:
+            bigger_child_index = self.storage.index(bigger_child)
+            self.storage[bigger_child_index], self.storage[index] = self.storage[index], self.storage[bigger_child_index]
+            return self._sift_down(self.storage.index(bigger_child) + 1)
+
+    def _get_bigger_child(self, index):
+        right_child = None
+        left_child = None
+        if (index * 2) + 1 < len(self.storage):
+            left_child = self.storage[(index * 2) + 1]
+        if (index * 2) + 2 < len(self.storage):
+            right_child = self.storage[(index * 2) + 2]
+        if right_child is not None and left_child is not None:
+            return max(right_child, left_child)
+        elif right_child is not None and left_child is None:
+            return right_child
+        elif right_child is None and left_child is not None:
+            return left_child
