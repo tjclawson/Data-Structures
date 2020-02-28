cd952514787e2b6f656b6fcd8e0a92062bd170e9
tjclawson <tjamesclawson@gmail.com>
2020-02-27 19:28:29 -0500

    Add generic heap solution

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -1,22 +1,54 @@
 class Heap:
-    def __init__(self, comparator):
+    def __init__(self, comparator=lambda x, y: x > y):
         self.storage = []
         self.comparator = comparator
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
-        pass
+        # swaps root and last element
+        self.storage[0], self.storage[len(self.storage) - 1] = self.storage[len(self.storage) - 1], self.storage[0]
+        # delete last element which is now the max
+        deleted_value = self.storage.pop()
+        # sifts down the new root element to ensure heap property is maintained
+        self._sift_down(0)
+        return deleted_value
 
     def get_priority(self):
-        pass
+        return self.storage[0]
 
     def get_size(self):
-        pass
+        return len(self.storage)
 
     def _bubble_up(self, index):
-        pass
+        parent_index = (index - 1) // 2
+        if parent_index >= 0 and self.comparator(self.storage[index], self.storage[parent_index]):
+            self.storage[index], self.storage[parent_index] = self.storage[parent_index], self.storage[index]
+            return self._bubble_up(parent_index)
 
     def _sift_down(self, index):
-        pass
+        bigger_child = self._get_correct_child(index)
+        if bigger_child is not None and self.comparator(bigger_child, self.storage[index]):
+            bigger_child_index = self.storage.index(bigger_child)
+            self.storage[bigger_child_index], self.storage[index] = self.storage[index], self.storage[
+                bigger_child_index]
+            return self._sift_down(self.storage.index(bigger_child) + 1)
+
+    def _get_correct_child(self, index):
+        right_child = None
+        left_child = None
+        if (index * 2) + 1 < len(self.storage):
+            left_child = self.storage[(index * 2) + 1]
+        if (index * 2) + 2 < len(self.storage):
+            right_child = self.storage[(index * 2) + 2]
+        if right_child is not None and left_child is not None:
+            if self.comparator(right_child, left_child):
+                return right_child
+            else:
+                return left_child
+        elif right_child is not None and left_child is None:
+            return right_child
+        elif right_child is None and left_child is not None:
+            return left_child
